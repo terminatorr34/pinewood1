dc06f39e626d94a770077f1f265dcf89e9419fb4
terminatorr34 <ander.kiim@gmail.com>
2023-03-04 17:06:44 +0400

    заверстал второй слайдер

--- js/main.js
+++ js/main.js
@@ -1,15 +1,10 @@
 
 const sliderContainer = document.querySelector('.slider-container')
-// const slider = document.querySelector('.slider__wrapper')
 const imgsSlider = document.querySelectorAll('.slider_item')
 const sliderLine = document.querySelector('.slider__items')
-console.log(imgsSlider.length)
 const arrow = document.querySelector('.slider_arrow')
-// console.log (sliderContainer.offsetWidth)
-
 
 let width
-// console.log (width)
 function init() {
     width =  sliderContainer.offsetWidth;
     sliderLine.style.width = width * imgsSlider.length + 'px';
@@ -17,15 +12,13 @@ function init() {
         Element.style.width = width  + 'px';
         Element.style.height = 'auto';
     })
- console.log (width)
+
 }
 init()
 
 window.addEventListener('resize', function () {
     init()
     sliderMoveRight()
-    // this.location.reload()
-    
 })
 
 let count1 = 0
@@ -34,8 +27,6 @@ arrow.addEventListener('click', function() {
             count1++
         else if (count1 >= imgsSlider.length - 1)
             count1 = 0
-        // else if (count1 == sliderImages.length)
-        //     count1 = 0;
 
         else
             count1 = 1;
@@ -43,13 +34,10 @@ arrow.addEventListener('click', function() {
 }
 )
 
-
 function sliderMoveRight () {
-    console.log('sdfsd')
 sliderLine.style.transform = 'translate(-' + width * count1 + 'px)';
 }
 
 window.addEventListener('resize', function () {
-    init()
-    
+    init()  
 })
